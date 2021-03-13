695f39550fa5639ab6651a2bc4821323a59f31f1
innocent2102 <michal2102@gmail.com>
2021-03-13 20:36:46 +0100

    feat: init Camera component with unit test

---
+++ src/components/Camera/Camera.spec.ts
@@ -0,0 +1,17 @@
+import { shallowMount } from '@vue/test-utils';
+import Camera from './Camera.vue';
+
+describe('Camera.vue', () => {
+  let wrapper: any;
+  beforeEach(() => {
+    wrapper = shallowMount(Camera, {});
+  });
+
+  it('Should be defined', () => {
+    expect(wrapper.is(Camera)).not.toBeUndefined();
+  });
+
+  it('Should match snapshot', () => {
+    expect(wrapper.element).toMatchSnapshot();
+  });
+})
