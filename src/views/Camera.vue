929d831c5a2c11779fb4533fc66a5170c9bbaa87
innocent2102 <michal2102@gmail.com>
2021-03-13 19:46:26 +0100

    feat: init Camera component

---
+++ src/views/Camera.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2>Camera</h2>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {},
+})
+export default class Camera extends Vue {}
+</script>
