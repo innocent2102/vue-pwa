70251b51bb23d753661d479f55a7e86270c3508e
innocent2102 <michal2102@gmail.com>
2021-03-13 23:06:59 +0100

    chore: add assets alias to webpack config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
+const path = require('path');
+
 module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "assets": path.resolve(__dirname, 'src/assets')
+      }
+    }
+  },
   transpileDependencies: [
     'vuetify'
   ]
