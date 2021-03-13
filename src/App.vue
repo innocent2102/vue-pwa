83a91bb33709b4f11562743978015871eb643d5b
innocent2102 <michal2102@gmail.com>
2021-03-13 14:16:47 +0100

    feat: add side nav-menu

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,109 @@
 <template>
-    <v-app>
-`      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
+<!--    <v-app>-->
+<!--      <router-link to="/">Home</router-link>-->
+<!--      <router-link to="/about">About</router-link>-->
+<!--      <router-view/>-->
+<!--    </v-app>-->
+
+
+  <div class="pwa-app__main-container">
+    <v-card
+      height="100%"
+      width="256"
+      class="mx-auto"
+    >
+      <nav>
+        <v-navigation-drawer permanent>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="title">
+                Vue-PWA
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                MEAN
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider></v-divider>
+
+          <v-list
+            dense
+            nav
+          >
+            <v-list-item
+              v-for="item in items"
+              :key="item.title"
+              link
+            >
+              <router-link :to="item.url">
+                <div class="pwa-app__menu-item">
+                  <v-list-item-icon>
+                    <v-icon>{{ item.icon }}</v-icon>
+                  </v-list-item-icon>
+
+                  <v-list-item-content>
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                  </v-list-item-content>
+                </div>
+              </router-link>
+
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+      </nav>
+
+    </v-card>
+    <div class="pwa-app__router-container">
       <router-view/>
-    </v-app>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import { Component, Vue } from 'vue-property-decorator';
 
-export default Vue.extend({
-  name: 'App',
 
-  components: {
-  },
-
-  data: () => ({
-    //
-  }),
-});
+@Component({
+  components: {},
+})
+export default class Home extends Vue {
+  items = [
+    { title: 'Home', icon: 'mdi-image', url: '/' },
+    { title: 'About', icon: 'mdi-help-box', url: '/about' },
+  ];
+  right = null;
+}
 </script>
+
+<style lang="scss">
+html,
+body {
+  height: 100%;
+}
+
+a, a:link, a:visited, a:active, a:hover {
+  color: black;
+  text-decoration: none;
+  width: 100%;
+}
+
+.v-list-item__icon {
+  padding: 0 12px 0 0;
+}
+
+.pwa-app {
+  &__main-container {
+    height: 100%;
+    display: flex;
+  }
+
+  &__router-container {
+    padding: 12px;
+  }
+
+  &__menu-item {
+    display: flex;
+  }
+}
+</style>
